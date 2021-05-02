5b814eee1847cb7da352a13803b749881b91c1ad
Simon Meier <meiersi0@students.zhaw.ch>
2021-05-02 17:50:53 +0200

    task 4 completed

--- week2.R
+++ week2.R
@@ -12,15 +12,15 @@ wildschwein_BE <- st_as_sf(wildschwein_BE, coords = c("E", "N"), crs = 2056, rem
 wildschwein_BE
 
 #Task 1
+#Getting an overview
 wildschwein_BE <- group_by(wildschwein_BE,TierID)
-wildschwein_BE <- mutate(wildschwein_BE,timelag = as.numeric(difftime(DatetimeUTC, lead(DatetimeUTC), units = "secs")))
-wildschwein_BE <- mutate(wildschwein_BE,timelag = as.integer(difftime(DatetimeUTC, lead(DatetimeUTC), units = "secs")))
-
+wildschwein_BE <- mutate(wildschwein_BE,timelag = as.integer(difftime(DatetimeUTC, lag(DatetimeUTC), units = "secs")))
 wildschwein_BE
 
 #How many individuals were tracked? -->3
 str(wildschwein_BE)
-  
+ggplot(wildschwein_BE, aes(DatetimeUTC, TierID))+
+  geom_boxplot()
 
 #For how long were the individual tracked? Are there gaps? --> Wieso geht das nicht?
 wildschwein_BE %>%
@@ -28,13 +28,16 @@ wildschwein_BE %>%
   summarise(         
     total_obs = sum(timelag)
   )
-    
+
+plot(timelag~DatetimeUTC, wildschwein_BE)
+
 #Were all individuals tracked concurrently or sequentially? --> Most of the time Yes (1st Qu. 896 3rd QU. 916 sec) But there are Exceptions
 summary(wildschwein_BE)
 
 #What is the temporal sampling interval between the locations? --> Mean 1408 sec
 
 #Task 2 
+#Deriving movement parameters I: Speed
 #calculate Steplength
 
 wildschwein_BE <- wildschwein_BE %>%
@@ -50,10 +53,11 @@ wildschwein_BE <- wildschwein_BE %>%
 wildschwein_BE <- wildschwein_BE %>%
   mutate(speed = (steplength_tot/timelag))
 
-#What speed unit do you get? m/s
+#What speed unit do you get? --> m/s
 
 
 #Task 3
+#Cross-scale movement analysis
 caro <- read_delim("caro60.txt",",")
 caro
 
@@ -155,6 +159,24 @@ ggplot(caro_rbind_all, aes(DatetimeUTC, speed, colour=TierName))+
   labs(title = "comparing derived speed at different sampling intervals")
 
 
+#Task 4
+#Deriving movement parameters II: Rolling window functions
+install.packages("zoo")
+library(zoo)
+
+example <- rnorm(10)
+rnorm
+rollmean(example,k = 3,fill = NA,align = "left")
+rollmean(example,k = 4,fill = NA,align = "left")
+
+roll_3 <- rollmean(caro$speed,k = 3,fill = NA,align = "left")
+roll_3
+roll_6 <- rollmean(caro$speed,k = 6,fill = NA,align = "left")
+roll_9 <- rollmean(caro$speed,k = 9,fill = NA,align = "left")
+
+plot(roll_3)
+plot(roll_6)
+plot(roll_9)
 
 
 
