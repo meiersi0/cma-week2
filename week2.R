af44fa76ff7ae7173bdbf9a3e5ddc6fd7856b903
Simon Meier <meiersi0@students.zhaw.ch>
2021-05-02 17:10:04 +0200

    Task 3 completed

--- week2.R
+++ week2.R
@@ -13,8 +13,8 @@ wildschwein_BE
 
 #Task 1
 wildschwein_BE <- group_by(wildschwein_BE,TierID)
-wildschwein_BE <- mutate(wildschwein_BE,timelag = as.numeric(difftime(lead(DatetimeUTC),DatetimeUTC, units = "secs")))
-wildschwein_BE <- mutate(wildschwein_BE,timelag = as.integer(difftime(lead(DatetimeUTC),DatetimeUTC, units = "secs")))
+wildschwein_BE <- mutate(wildschwein_BE,timelag = as.numeric(difftime(DatetimeUTC, lead(DatetimeUTC), units = "secs")))
+wildschwein_BE <- mutate(wildschwein_BE,timelag = as.integer(difftime(DatetimeUTC, lead(DatetimeUTC), units = "secs")))
 
 wildschwein_BE
 
@@ -37,19 +37,17 @@ summary(wildschwein_BE)
 #Task 2 
 #calculate Steplength
 
-wildschwein_BE_steplength <- wildschwein_BE %>%
+wildschwein_BE <- wildschwein_BE %>%
   group_by(TierID) %>%
-  mutate(steplength_E = E - lag(E))%>%#Question: Why did we use lead and nod lag?
+  mutate(steplength_E = E - lag(E))%>%
   mutate(steplength_N = N - lag(N))
 
-wildschwein_BE_steplength_tot <- wildschwein_BE_steplength %>%
+wildschwein_BE <- wildschwein_BE %>%
   mutate(steplength_tot = (sqrt((steplength_E*steplength_E)+(steplength_N*steplength_N))))
-
-rm(wildschwein_BE_steplength) 
 
 
 #calculate speed
-wildschwein_BE_speed <- wildschwein_BE_steplength_tot %>%
+wildschwein_BE <- wildschwein_BE %>%
   mutate(speed = (steplength_tot/timelag))
 
 #What speed unit do you get? m/s
@@ -66,10 +64,95 @@ caro_6 <- caro%>%
 caro_9 <- caro%>%
   slice(which(row_number() %% 9 == 1))
 
+#caro
+#Calculate timelag
+caro <- mutate(caro,timelag = as.integer(difftime(DatetimeUTC,lag(DatetimeUTC), units = "secs")))
 
+#calculate Steplength
+caro <- caro %>%
+  mutate(steplength_E = E - lag(E))%>%
+  mutate(steplength_N = N - lag(N))
+caro <- caro %>%
+  mutate(steplength_tot = (sqrt((steplength_E*steplength_E)+(steplength_N*steplength_N))))
 
+#calculate speed
+caro <- caro %>%
+  mutate(speed = (steplength_tot/timelag))
 
+#caro_3
+caro_3 <- mutate(caro_3,timelag = as.integer(difftime(DatetimeUTC,lag(DatetimeUTC), units = "secs")))
 
+caro_3 <- caro_3 %>%
+  mutate(steplength_E = E - lag(E))%>%
+  mutate(steplength_N = N - lag(N))
+caro_3 <- caro_3 %>%
+  mutate(steplength_tot = (sqrt((steplength_E*steplength_E)+(steplength_N*steplength_N))))
+
+caro_3 <- caro_3 %>%
+  mutate(speed = (steplength_tot/timelag))
+
+#caro_6
+caro_6 <- mutate(caro_6,timelag = as.integer(difftime(DatetimeUTC,lag(DatetimeUTC), units = "secs")))
+
+caro_6 <- caro_6 %>%
+  mutate(steplength_E = E - lag(E))%>%
+  mutate(steplength_N = N - lag(N))
+caro_6 <- caro_6 %>%
+  mutate(steplength_tot = (sqrt((steplength_E*steplength_E)+(steplength_N*steplength_N))))
+
+caro_6 <- caro_6 %>%
+  mutate(speed = (steplength_tot/timelag))
+
+#caro_9
+caro_9 <- mutate(caro_9,timelag = as.integer(difftime(DatetimeUTC,lag(DatetimeUTC), units = "secs")))
+
+caro_9 <- caro_9 %>%
+  mutate(steplength_E = E - lag(E))%>%
+  mutate(steplength_N = N - lag(N))
+caro_9 <- caro_9 %>%
+  mutate(steplength_tot = (sqrt((steplength_E*steplength_E)+(steplength_N*steplength_N))))
+
+caro_9 <- caro_9 %>%
+  mutate(speed = (steplength_tot/timelag))
+
+#compare caro to caro_3
+caro_3 <- caro_3%>% mutate(TierName = "Caro_3")
+caro_rbind_3 <- rbind(caro, caro_3)
+
+ggplot(caro_rbind_3, aes(E,N, colour = TierName)) +
+  geom_point()+
+  geom_path()+
+  theme(aspect.ratio=1)+
+  labs(title = "comparing original- with 3 minutes-resampled data")
+  
+#compare caro to caro_6
+caro_6 <- caro_6%>% mutate(TierName = "Caro_6")
+caro_rbind_6 <- rbind(caro, caro_6)
+
+ggplot(caro_rbind_6, aes(E,N, colour = TierName)) +
+  geom_point()+
+  geom_path()+
+  theme(aspect.ratio=1)+
+  labs(title = "comparing original- with 6 minutes-resampled data")
+
+#compare caro to caro_9
+caro_9 <- caro_9%>% mutate(TierName = "Caro_9")
+caro_rbind_9 <- rbind(caro, caro_9)
+
+ggplot(caro_rbind_9, aes(E,N, colour = TierName)) +
+  geom_point()+
+  geom_path()+
+  theme(aspect.ratio=1)+
+  labs(title = "comparing original- with 9 minutes-resampled data")
+  
+  
+#compare speed
+caro_rbind_all <- rbind(caro, caro_3, caro_6, caro_9)
+
+ggplot(caro_rbind_all, aes(DatetimeUTC, speed, colour=TierName))+
+  geom_point()+
+  geom_path()+
+  labs(title = "comparing derived speed at different sampling intervals")
 
 
 
